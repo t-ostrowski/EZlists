ecbc3d631bc95ff2adc7584e347f42f79a16cd56
Thomas Ostrowski <ostrowski.thomas@gmail.com>
2016-08-25 16:19:01 +0200

    Add splash screen

---
+++ app/src/main/java/com/ostro/ezlists/ui/SplashActivity.java
@@ -0,0 +1,87 @@
+package com.ostro.ezlists.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.ostro.ezlists.base.BaseActivity;
+import com.ostro.ezlists.ui.list.DetailsListActivity;
+
+/**
+ * Created by Thomas Ostrowski
+ * ostrowski.thomas@gmail.com
+ * on 25/08/2016.
+ */
+
+public class SplashActivity extends BaseActivity {
+
+    private Handler mHandler;
+
+    private boolean splashScreenIsFinished = false;
+    private boolean isLookingForUser = false;
+    private boolean activityStopped = false;
+    private boolean enableSplash = true;
+
+    @Override
+    public int getLayoutResources() {
+        return 0;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mHandler = new Handler(Looper.getMainLooper());
+        if (enableSplash) {
+            mHandler.postDelayed(startActivity, 700);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        activityStopped = false;
+        // find existing account
+        chooseActivity();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        splashScreenIsFinished = true;
+        activityStopped = true;
+    }
+
+    private void startDetailsListActivity() {
+        isLookingForUser = false;
+        launchActivity(new Intent(this, DetailsListActivity.class));
+    }
+
+    private Runnable startActivity = new Runnable() {
+        @Override
+        public void run() {
+            splashScreenIsFinished = true;
+            if (isLookingForUser) {
+                return;
+            }
+            chooseActivity();
+        }
+    };
+
+    private void launchActivity(Intent intent) {
+        if (enableSplash) {
+            mHandler.removeCallbacks(startActivity);
+        }
+        startActivity(intent);
+        finish();
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+
+    private boolean chooseActivity() {
+        if (enableSplash && (!splashScreenIsFinished || activityStopped)) {
+            return false;
+        }
+        startDetailsListActivity();
+        return true;
+    }
+}
