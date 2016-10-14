5e43941a85faa3603fceb6aef525133af14ff2bc
Thomas Ostrowski <ostrowski.thomas@gmail.com>
2016-10-14 17:48:24 +0200

    Update realm version + edit init app

--- app/src/main/java/com/ostro/ezlists/EZlistsApp.java
+++ app/src/main/java/com/ostro/ezlists/EZlistsApp.java
@@ -1,7 +1,12 @@
 package com.ostro.ezlists;
 
 import android.app.Application;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 import timber.log.Timber;
@@ -16,15 +21,55 @@ public class EZlistsApp extends Application {
 
     private static EZlistsApp INSTANCE;
 
+    public static EZlistsApp get() {
+        return INSTANCE;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
-        Timber.plant(new Timber.DebugTree());
-        RealmConfiguration realmConfig = new RealmConfiguration.Builder(this).build();
-        Realm.setDefaultConfiguration(realmConfig);
+        INSTANCE = this;
+        initializeLogger();
+        initializeRealm();
     }
 
-    public static EZlistsApp get() {
-        return INSTANCE;
+    private void initializeLogger() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        } else {
+            Fabric.with(this, new Crashlytics());
+            Timber.plant(new CrashlyticsTree());
+        }
+    }
+
+    private void initializeRealm() {
+        Realm.init(this);
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder().build();
+        Realm.setDefaultConfiguration(realmConfiguration);
+    }
+
+    public class CrashlyticsTree extends Timber.Tree {
+        private static final String CRASHLYTICS_KEY_PRIORITY = "priority";
+        private static final String CRASHLYTICS_KEY_TAG = "tag";
+        private static final String CRASHLYTICS_KEY_MESSAGE = "message";
+
+        @Override
+        protected void log(int priority, @Nullable String tag, @Nullable String message,
+                           @Nullable Throwable exception) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG || priority == Log.INFO
+                    || priority == Log.WARN) {
+                return;
+            }
+
+            Crashlytics.setInt(CRASHLYTICS_KEY_PRIORITY, priority);
+            Crashlytics.setString(CRASHLYTICS_KEY_TAG, tag);
+            Crashlytics.setString(CRASHLYTICS_KEY_MESSAGE, message);
+
+            if (exception == null) {
+                Crashlytics.logException(new Exception(message));
+            } else {
+                Crashlytics.logException(exception);
+            }
+        }
     }
 }
